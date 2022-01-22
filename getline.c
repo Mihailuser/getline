7b2ca4827f4194eeba5153b026fe7d7cc30aca02
Mikheil <60305561+Mihailuser@users.noreply.github.com>
2022-01-22 18:04:17 +1000

    Update getline.c

--- getline.c
+++ getline.c
@@ -4,6 +4,10 @@
 char *getline(FILE *f, unsigned int max_str_len){
     char *res = malloc(max_str_len);
     char c;
+    if(f == NULL){
+        perror("Error");
+        return 0;
+    }
     for(int i = 0; c!='\n'; i++){
         c = getc(f);
         res[i] = c;
